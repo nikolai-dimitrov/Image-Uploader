ca36bd041d7418ae480ceadd0781ff7a12edde1a
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2026-01-15 15:55:21 +0200

    feat: add cors middleware to prevent blocking requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
-require('dotenv').config()
+require("dotenv").config();
 
 app = express();
 
 async function startApp(app) {
 	try {
+		app.use(cors());
 		app.use(routes);
 
 		app.listen(3000, () => {
